5bd033549db8a035fab079f48a93d3b16df3dc8b
AjitJadhav1 <ajitjadhav482002@gmail.com>
2024-11-21 14:47:48 +0000

    learn about variabes

---
+++ BasicsOfJS/variables.js
@@ -0,0 +1,19 @@
+const id = 33;
+let myName = 1;
+var nickName = "aj";
+city = "latur"
+
+
+// const id = 22;   not allowed
+
+/*
+
+prefer to use let due to scope and functional issues in js
+
+*/
+console.table([id, myName, nickName,city]);
+
+console.log(typeof id)
+console.log(typeof myName)
+console.log(typeof nickName)
+console.log(typeof city)
